2e319302acaeb11c3f17a4b57635dc755cd608ef
roherrerap <roherrerap@unal.edu.co>
2015-10-21 20:45:32 -0500

    Update - DomClass: User, Admin, Regular, TestClass: User, Admin, Regular

--- grails-app/domain/forotallersd/User.groovy
+++ grails-app/domain/forotallersd/User.groovy
@@ -8,10 +8,10 @@ class User {
     String password
 
     static constraints = {
-        name (blank: false, nullable: false, minSize: 3, maxSize: 50)
-        lastname (blank: false, nullable: false, minSize: 3, maxSize: 50)
+        name (blank: false, nullable: false, size: 3..50)
+        lastname (blank: false, nullable: false, size: 3..50)
         age (blank: false, nullable: false, min: 14)
         username (blank: false, nullable: false, unique: true)
-        password blank: false, nullable: false, password: true, minSize: 8, matches: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$'
+        password (blank: false, nullable: false, password: true, minSize: 8, matches: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$')
     }
 }
