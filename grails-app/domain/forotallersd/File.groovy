90d9e9cf35e6fe727336bb0d4c2cc0f6db636fc7
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2015-10-21 22:32:58 -0500

    Se escribe el test de unidad para File.

--- grails-app/domain/forotallersd/File.groovy
+++ grails-app/domain/forotallersd/File.groovy
@@ -6,7 +6,7 @@ class File {
     double size
 
     static constraints = {
-        fileType matches: "[a-zA-Z]+\/[a-zA-Z]+", nullable: false, blank: false
+        fileType matches: "[a-zA-Z]+/[a-zA-Z]+", nullable: false, blank: false
         content nullable:false, blank:false
         size max: 10000000, nullable: false, blank: false
     }
