db047fb519b0badd35c01b06ef078b649460e232
Jefersson <jfcanob@unal.edu.co>
2015-10-21 22:39:38 -0500

    Update Test for Forum Domain Class

--- test/unit/forotallersd/ForumSpec.groovy
+++ test/unit/forotallersd/ForumSpec.groovy
@@ -10,11 +10,65 @@ import spock.lang.Specification
 class ForumSpec extends Specification {
 
     def setup() {
+		mockForConstraintsTests(Forum)
     }
 
     def cleanup() {
     }
+	
+	@Unroll("test Forum attribute name: #name, field: #field, valid: #valid")
+    def "Testing Domain Class Forum - name"() {
+		when:
+		def t_forum_name = new Forum(name: name, dateCreated: new Date(), category: "12")
+		t_forum_name.validate()
+		
+		then:
+		t_forum_name.errors.hasFieldErrors("name") == false
 
-    void "test something"() {
+		where:
+		name	| valid		| field
+		'bc'	| false		| 'minSize'
+		null	| false		| 'null'
+		'abdc'	| true		| 'size'
+		'aaaaaaaaaaeeeeeeeeeeiiiiiiiiiioooooooooouuuuuuuuuu'	| true		| 'size'
+		'zzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvv1'	| false		| 'maxSize'
     }
+	
+	@Unroll("test Forum attribute dateCreated: #dateCreated, field: #field, valid: #valid")
+	def "Testing Domain Class Forum - dateCreated"(){
+		when:
+		def t_forum_dateCreated = new Forum(name: "Prueba", dateCreated: dateCreated, category: "4")
+		t_forum_dateCreated.validate()
+
+		then:
+		t_forum_dateCreated.hasFieldErrors("dateCreated") == false
+
+		where:
+		dateCreated 	| valid		| field
+		null			| false		| 'null'
+		new Date()		| true		| 'min'
+		new Date()-2	| false		| 'minValue'	
+	}
+	@Unroll("test Forum attribute category: #category, field: #field, valid: #valid")
+	def "Testing Domain Class Forum - category"(){
+		when:
+		def t_forum_category = new Forum(name: "Prueba", dateCreated: new Date(), category: category)
+
+		then:
+		t_forum_category.hasFieldError("category") == false
+
+		where:
+		category	| valid		| field
+		'ab'		| false 	| 'minSize'
+		null		| false		| null
+		'xyz'		| true		| 'size'
+		'hhhiiijjjkkklll'	| true	| 'size'
+		'hhhiiijjjkkklllt'	| false	| 'maxSize'
+
+
+
+
+
+
+	}
 }
