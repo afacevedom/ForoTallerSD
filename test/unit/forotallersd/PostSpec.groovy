7410c1792de6026352f1e90c69ac3e0913ad9298
eabohorquezg <eabohorquezg@unal.edu.co>
2015-10-21 23:53:26 -0500

    Se crea test para la clase Post

--- test/unit/forotallersd/PostSpec.groovy
+++ test/unit/forotallersd/PostSpec.groovy
@@ -2,19 +2,64 @@ package forotallersd
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
 @TestFor(Post)
 class PostSpec extends Specification {
 
     def setup() {
+        mockForConstraintsTests(Post)
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    @Unroll ("test Post attribute topic: #topic, field: #field, valid: #valid ")
+    def "Testing Domain Class Post - topic"(){
+        when:
+        def obj = new Post(topic: topic,dateCreated: new Date(), lastUpdate: new Date(),itsAllowed: true)
+        obj.validate()
+
+        then:
+        obj.errors.hasFieldErrors("topic") == false
+
+        where:
+        name   | valid | field
+        'ab'   | false | 'minSize'
+        'abc'  | true  | 'minSize'
+        'abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde'  | false | 'maxSize'
+        'abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde'  | true | 'maxSize'
     }
+
+    @Unroll("test Post attribute dateCreated: #dateCreated, field: #field, valid: #valid")
+    def "Testing Domain Class Post - dateCreated"(){
+        when:
+        def obj = new Post(topic: "programacion",dateCreated: dateCreated, lastUpdate: new Date(),itsAllowed: true)
+        obj.validate()
+
+        then:
+        obj.hasFieldErrors("dateCreated") == false
+
+        where:
+        dateCreated 	| valid		| field
+        new Date()		| true		| 'min'
+        new Date()-3		| false		| 'min'
+
+    }
+
+    @Unroll("test Post attribute lastUpdate: #lastUpdate, field: #field, valid: #valid")
+    def "Testing Domain Class Post - lastUpdate"(){
+        when:
+        def obj = new Post(topic: "programacion",dateCreated: new Date(), lastUpdate: lastUpdate,itsAllowed: true)
+        obj.validate()
+
+        then:
+        obj.hasFieldErrors("lastUpdate") == false
+
+        where:
+        dateCreated 	| valid		| field
+        new Date()		| true		| 'min'
+        new Date()-3		| false		| 'min'
+    }
+
 }
