eff6de77c8e4e1c47e35c8aa8ca52f05c8dd5792
Jefersson <jfcanob@unal.edu.co>
2015-10-24 20:42:17 -0500

    Por lo cual se modifica la clase Admin para que retorne las consultas ordenados por el nivel del Administrador

--- grails-app/domain/forotallersd/Admin.groovy
+++ grails-app/domain/forotallersd/Admin.groovy
@@ -3,9 +3,15 @@ package forotallersd
 class Admin extends User {
     Integer level
     Double rating
-
+	
+	static hasMany = [forum:Forum]
+	
     static constraints = {
         level (nullable: false, range: 1..5)
         rating (nullable: false, range: 0..100)
     }
+
+	static mapping = {
+		sort "level"
+	}
 }
