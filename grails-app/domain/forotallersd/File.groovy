f2986a3c387af93cdc0c4dbf6047bd778ea2ad69
roherrerap <roherrerap@unal.edu.co>
2015-10-24 21:22:51 -0500

    Update - Domain Class: File and Post, Changes: methods before(insert,update), delete, relationship

--- grails-app/domain/forotallersd/File.groovy
+++ grails-app/domain/forotallersd/File.groovy
@@ -5,9 +5,11 @@ class File {
     byte[] content
     double size
 
+    static belongsTo = [post_belongs_id : Post]
+
     static constraints = {
         fileType matches: "[a-zA-Z]+/[a-zA-Z]+", nullable: false, blank: false
         content nullable:false, blank:false
-        size max: 10000000, nullable: false, blank: false
+        size maxSize: 10*1024*1024, nullable: false, blank: false
     }
 }
--- grails-app/domain/forotallersd/Post.groovy
+++ grails-app/domain/forotallersd/Post.groovy
@@ -5,9 +5,25 @@ class Post {
     Date dateCreated
     Date lastUpdate
     Boolean itsAllowed
-    
-    static belongsTo = [regular: Regular]
-    static belongstTo = [forum: Forum]
+
+    static hasMany = [owner_id : File]
+
+    static belongsTo = [fatherForum_id : Forum, regular: Regular]
+
+    def beforeInsert(){
+        this.dateCreated = new Date()
+    }
+
+    def beforeUpdate(){
+        this.lastUpdate = new Date()
+    }
+
+    def delete(){
+        def posts = Post.get(this)
+        def files = File.findAllByPost_belongs_id(posts)
+        files*.delete()
+        posts.delete()
+    }
 
     static constraints = {
         topic (nullable: false, minSize: 3, maxSize: 50)
